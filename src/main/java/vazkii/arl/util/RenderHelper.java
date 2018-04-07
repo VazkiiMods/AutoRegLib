55be98eabf7b90fa079306e7429dbdc464990bb5
Vasco Lavos <vazkii@hotmail.com>
2018-04-08 00:08:16 +0100

    Star render

--- src/main/java/vazkii/arl/util/RenderHelper.java
+++ src/main/java/vazkii/arl/util/RenderHelper.java
@@ -11,6 +11,7 @@
 package vazkii.arl.util;
 
 import java.util.List;
+import java.util.Random;
 
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.GL11;
@@ -154,6 +155,64 @@ public final class RenderHelper {
 		buff.pos(par1 + par5, par2 + 0, z).tex((par3 + par5) * f, (par4 + 0) * f1).endVertex();
 		buff.pos(par1 + 0, par2 + 0, z).tex((par3 + 0) * f, (par4 + 0) * f1).endVertex();
 		tessellator.draw();
+	}
+	
+	public static void renderStar(int color, float scale, long seed) {
+		renderStar(color, scale, scale, scale, seed);
+	}
+	
+	public static void renderStar(int color, float xScale, float yScale, float zScale, long seed) {
+		Tessellator tessellator = Tessellator.getInstance();
+
+		float ticks = (ClientTicker.ticksInGame % 200) + ClientTicker.partialTicks;
+		if (ticks >= 100)
+			ticks = 200 - ticks - 1;
+
+		float f1 = ticks / 200F;
+		float f2 = 0F;
+		if (f1 > 0.7F)
+			f2 = (f1 - 0.7F) / 0.2F;
+		Random random = new Random(seed);
+
+		GlStateManager.pushMatrix();
+		GlStateManager.disableTexture2D();
+		GlStateManager.shadeModel(GL11.GL_SMOOTH);
+		GlStateManager.enableBlend();
+		GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE);
+		GlStateManager.disableAlpha();
+		GlStateManager.enableCull();
+		GlStateManager.depthMask(false);
+		GlStateManager.scale(xScale, yScale, zScale);
+
+		for (int i = 0; i < (f1 + f1 * f1) / 2F * 90F + 30F; i++) {
+			GlStateManager.rotate(random.nextFloat() * 360F, 1F, 0F, 0F);
+			GlStateManager.rotate(random.nextFloat() * 360F, 0F, 1F, 0F);
+			GlStateManager.rotate(random.nextFloat() * 360F, 0F, 0F, 1F);
+			GlStateManager.rotate(random.nextFloat() * 360F, 1F, 0F, 0F);
+			GlStateManager.rotate(random.nextFloat() * 360F, 0F, 1F, 0F);
+			GlStateManager.rotate(random.nextFloat() * 360F + f1 * 90F, 0F, 0F, 1F);
+			tessellator.getBuffer().begin(GL11.GL_TRIANGLE_FAN, DefaultVertexFormats.POSITION_COLOR);
+			float f3 = random.nextFloat() * 20F + 5F + f2 * 10F;
+			float f4 = random.nextFloat() * 2F + 1F + f2 * 2F;
+			float r = ((color & 0xFF0000) >> 16) / 255F;
+			float g = ((color & 0xFF00) >> 8) / 255F;
+			float b = (color & 0xFF) / 255F;
+			tessellator.getBuffer().pos(0, 0, 0).color(r, g, b, 1F - f2).endVertex();
+			tessellator.getBuffer().pos(-0.866D * f4, f3, -0.5F * f4).color(0, 0, 0, 0).endVertex();
+			tessellator.getBuffer().pos(0.866D * f4, f3, -0.5F * f4).color(0, 0, 0, 0).endVertex();
+			tessellator.getBuffer().pos(0, f3, 1F * f4).color(0, 0, 0, 0).endVertex();
+			tessellator.getBuffer().pos(-0.866D * f4, f3, -0.5F * f4).color(0, 0, 0, 0).endVertex();
+			tessellator.draw();
+		}
+
+		GlStateManager.depthMask(true);
+		GlStateManager.disableCull();
+		GlStateManager.disableBlend();
+		GlStateManager.shadeModel(GL11.GL_FLAT);
+		GlStateManager.color(1F, 1F, 1F, 1F);
+		GlStateManager.enableTexture2D();
+		GlStateManager.enableAlpha();
+		GlStateManager.popMatrix();
 	}
 
 	public static String getKeyDisplayString(String keyName) {
