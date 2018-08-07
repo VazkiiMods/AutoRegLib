01331cbea396b157fc20a8211a75df623d32dc71
Vasco Lavos <vazkii@hotmail.com>
2018-08-07 19:54:57 +0100

    Implement rotation handler

---
+++ src/main/java/vazkii/arl/util/RotationHandler.java
@@ -0,0 +1,25 @@
+package vazkii.arl.util;
+
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.Rotation;
+
+public final class RotationHandler {
+
+	private static final Rotation[] FACING_TO_ROTATION = new Rotation[] {
+		Rotation.NONE,
+		Rotation.NONE,
+		Rotation.NONE,
+		Rotation.CLOCKWISE_180,
+		Rotation.COUNTERCLOCKWISE_90,
+		Rotation.CLOCKWISE_90
+	};
+	
+	public static EnumFacing rotateFacing(EnumFacing facing, Rotation rot) {
+		return rot.rotate(facing);
+	}
+	
+	public static EnumFacing rotateFacing(EnumFacing facing, EnumFacing rot) {
+		return rotateFacing(facing, FACING_TO_ROTATION[rot.ordinal()]);
+	}
+	
+}
