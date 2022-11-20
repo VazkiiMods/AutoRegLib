9f43eaa203b25dce7ae17cc6e2eeada7a78a2238
Vasco Lavos <vazkii@hotmail.com>
2022-11-20 13:26:59 +0000

    update for latest forge

--- src/main/java/vazkii/arl/util/ARLClientInitializer.java
+++ src/main/java/vazkii/arl/util/ARLClientInitializer.java
@@ -1,7 +1,7 @@
 package vazkii.arl.util;
 
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ColorHandlerEvent;
+import net.minecraftforge.client.event.RegisterColorHandlersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import vazkii.arl.AutoRegLib;
@@ -9,12 +9,12 @@ import vazkii.arl.AutoRegLib;
 @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = AutoRegLib.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ARLClientInitializer {
 	@SubscribeEvent
-	public static void registerBlockColors(ColorHandlerEvent.Block evt) {
+	public static void registerBlockColors(RegisterColorHandlersEvent.Block evt) {
 		RegistryHelper.submitBlockColors(evt.getBlockColors()::register);
 	}
 
 	@SubscribeEvent
-	public static void registerItemColors(ColorHandlerEvent.Item evt) {
+	public static void registerItemColors(RegisterColorHandlersEvent.Item evt) {
 		RegistryHelper.submitItemColors(evt.getItemColors()::register);
 	}
 }
